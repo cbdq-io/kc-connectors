1512bb443d7d366f10c06da4daba6780491d950d
Ben Dalling <b.dalling@cbdq.io>
2024-12-07 13:24:04 +0000

    fix: dev: Minor refactor.

--- tests/resources/poll-until-complete.sh
+++ tests/resources/poll-until-complete.sh
@@ -4,15 +4,15 @@ URL="http://localhost:9400/metrics"
 TARGET="azure_service_bus_sink_task_message_count_total 40000.0"
 
 while true; do
-    # Fetch the contents of the URL
-    RESPONSE=$(curl -s "$URL")
-    
-    # Check if the response contains the target string
-    if echo "$RESPONSE" | grep -q "$TARGET"; then
-        echo "Target string found: $TARGET"
-        break
-    fi
-    
-    # Wait for a short time before retrying
-    sleep 2
+	# Fetch the contents of the URL
+	RESPONSE=$(curl -s "$URL")
+
+	# Check if the response contains the target string
+	if echo "$RESPONSE" | grep -q "$TARGET"; then
+		echo "Target string found: $TARGET"
+		break
+	fi
+
+	# Wait for a short time before retrying
+	sleep 2
 done
