3c931a8f9be6e0548cf85183aa235123c39ec787
Ben Dalling <b.dalling@cbdq.io>
2024-12-27 15:28:34 +0000

    fix: Ensure connection string parser honours UseDevelopmentEmulator.

--- azure-servicebus-sink-connector/src/main/java/io/cbdq/ConnectionStringParser.java
+++ azure-servicebus-sink-connector/src/main/java/io/cbdq/ConnectionStringParser.java
@@ -6,15 +6,17 @@ public class ConnectionStringParser {
     private String endpoint;
     private String username;
     private String password;
+    private boolean useDevelopmentEmulator;
 
     public ConnectionStringParser(String connectionString) {
         this.connectionString = connectionString;
+        this.useDevelopmentEmulator = Boolean.parseBoolean(getValueFromConnectionString("UseDevelopmentEmulator", "false"));
         this.endpoint = getValueFromConnectionString("Endpoint");
         this.username = getValueFromConnectionString("SharedAccessKeyName");
         this.password = getValueFromConnectionString("SharedAccessKey");
     }
 
-    public String getValueFromConnectionString(String keyName) {
+    public String getValueFromConnectionString(String keyName, String... defaultValue) {
         String response = null;
         String[] parts = this.connectionString.split(";");
         String prefix = keyName + "=";
@@ -33,6 +35,10 @@ public class ConnectionStringParser {
         }
 
         if (response == null) {
+            if (defaultValue.length > 0) {
+                return defaultValue[0];
+            }
+
             String message = String.format("No %s found in the Azure Service Bus connection string.", keyName);
             throw new IllegalArgumentException(message);
         }
@@ -56,7 +62,12 @@ public class ConnectionStringParser {
 
         if (this.endpoint.startsWith("sb://")) {
             hostname = this.endpoint.substring("sb://".length());
-            protocol = "amqps://";
+
+            if (this.useDevelopmentEmulator) {
+                protocol = nonSslAMQP;
+            } else {
+                protocol = "amqps://";
+            }
         } else if (this.endpoint.startsWith(nonSslAMQP)) {
             hostname = this.endpoint.substring(nonSslAMQP.length());
             protocol = nonSslAMQP;
@@ -70,6 +81,11 @@ public class ConnectionStringParser {
         }
 
         brokerURL = protocol + hostname;
+
+        if (this.useDevelopmentEmulator) {
+            brokerURL = brokerURL + ":5672";
+        }
+
         return brokerURL;
     }
 
